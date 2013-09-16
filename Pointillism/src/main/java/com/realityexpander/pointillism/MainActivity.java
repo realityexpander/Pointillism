ee2fb9f75117483674ba41e8cfa7279dab42d8b9
realityexpander <realityexpander@gmail.com>
2013-09-16 12:58:49 -0500

    Add pick gallery

--- Pointillism/src/main/java/com/realityexpander/pointillism/MainActivity.java
+++ Pointillism/src/main/java/com/realityexpander/pointillism/MainActivity.java
@@ -1,17 +1,29 @@
 package com.realityexpander.pointillism;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
+        PickGalleryImage();
     }
 
+    public void PickGalleryImage(){
+        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        i.setType("image/*");
+        startActivityForResult(i, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
