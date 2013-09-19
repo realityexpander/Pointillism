bd9446d27858bba35ab2783952b42c8a9512ca15
Tom Fifelski <thomasfifelski@gmail.com>
2013-09-19 14:13:11 -0500

    adapter and header buttons

--- Pointillism/src/main/java/com/realityexpander/pointillism/MainActivity.java
+++ Pointillism/src/main/java/com/realityexpander/pointillism/MainActivity.java
@@ -1,11 +1,22 @@
 package com.realityexpander.pointillism;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ShareActionProvider;
+import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
+    private ShareActionProvider mShareActionProvider;
+
+
+String cool= "cool";
 
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 
@@ -15,8 +26,16 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+
         PickGalleryImage();
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void PickGalleryImage(){
@@ -25,11 +44,41 @@ public class MainActivity extends Activity {
         startActivityForResult(i, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
+
+
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.share_menu, menu);
+
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+
+        // Return true to display menu
+
         return true;
     }
-    
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+        shareIntent.getStringExtra(cool);
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
+    }
 }
